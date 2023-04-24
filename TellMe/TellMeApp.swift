e3d1517e5a875e8932fba2b2667a696fd48cc435
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-04-24 13:24:29 +0700

    Initial Commit

---
+++ TellMe/TellMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TellMeApp.swift
+//  TellMe
+//
+//  Created by Steven Wijaya on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TellMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
